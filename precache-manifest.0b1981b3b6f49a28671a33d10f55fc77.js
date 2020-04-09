9816c1040b046b71e356a0006ae79ff15f590065
unknown <luke.isitt@gmail.com>
2020-04-09 10:42:28 -0700

    Updates

---
+++ precache-manifest.0b1981b3b6f49a28671a33d10f55fc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bd9c484326c5a7a5300653f832c09a",
+    "url": "/isittluk/weather_react.git/index.html"
+  },
+  {
+    "revision": "bbc12d91b07dc68d57bf",
+    "url": "/isittluk/weather_react.git/static/css/main.41167b5a.chunk.css"
+  },
+  {
+    "revision": "e10816da91f7760321a0",
+    "url": "/isittluk/weather_react.git/static/js/2.087687ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/isittluk/weather_react.git/static/js/2.087687ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc12d91b07dc68d57bf",
+    "url": "/isittluk/weather_react.git/static/js/main.884c01c0.chunk.js"
+  },
+  {
+    "revision": "fad2274708710f1f3955",
+    "url": "/isittluk/weather_react.git/static/js/runtime-main.1e81ed16.js"
+  }
+]);
